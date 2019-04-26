b4b201b3b4030179fde8de41168cd700128d351c
Sarah Anderson <saranderson13@gmail.com>
2019-04-26 14:16:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.scan(/^[aeiou]/i).length == 1 ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text[0].scan(/[A-Z]/).length == 1 && text[-1].scan(/[[:punct:]]/).length == 1 ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  binding.pry
+  # currently only works for only numbers, punctuation breaks it.
+  # consider: delete all punctuation before scanning, or working possibilities into regex.
+  phone.scan(/(\d{3})+(\d{3})+(\d{4})/)
 end
