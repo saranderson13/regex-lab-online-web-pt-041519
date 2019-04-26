5d3d5892505c597fbea908f9b683986aa6c7fd0b
Sarah Anderson <saranderson13@gmail.com>
2019-04-26 14:46:29 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.scan(/^[aeiou]/i).length == 1 ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text[0].scan(/[A-Z]/).length == 1 && text[-1].scan(/[[:punct:]]/).length == 1 ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.gsub!(/[[:punct:]]|\s+/, "")
+  phone.scan(/(\d{3})+(\d{3})+(\d{4})\z/).length == 1 ? true : false
 end
